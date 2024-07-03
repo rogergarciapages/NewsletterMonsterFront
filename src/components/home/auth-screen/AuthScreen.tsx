f892e92ff13880d9a7ef52e544032ab257d6b500
Roger Garcia <96830104+rogergarciapages@users.noreply.github.com>
2024-07-03 15:36:25 +0100

    light / dark modes

--- src/components/home/auth-screen/AuthScreen.tsx
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import HeroSection from './HeroSection'
-import { TodaysHighlight } from './TodaysHighlight'
+import HeroSection from './HeroSection';
+import { TodaysHighlight } from './TodaysHighlight';
+import { ModeToggle } from '@/components/ModeToggle';
 
 const AuthScreen = () => {
   return (
     <div className="flex flex-col">
+        <ModeToggle />
         <HeroSection />
         <TodaysHighlight />
 
-        
-    
     </div>
-  )
-}
+  );
+};
 
-export default AuthScreen
+export default AuthScreen;
